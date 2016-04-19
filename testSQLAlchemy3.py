d4d67ac9b091392a768e715dced97e116a4b39fc
lbr0307 <lbr0307@126.com>
2016-04-19 00:13:03 -0400

    Update testSQLAlchemy3.py
    This is the refined version of SQLAlchemy test. It is using auto map method to map the tables in MySQL database to classes in python. Then we can use the mapping class to query databases, which is more convenient and easier to combine with GraphQL.

--- testSQLAlchemy3.py
+++ testSQLAlchemy3.py
@@ -9,70 +9,33 @@ SCHEMA_NAME = '2013snapshot'
 Base = declarative_base()
 engine = create_engine('mysql+mysqldb://root:sync@localhost')
 
-class People(Base):
-	__tablename__ = 'cb_people'
-	id = Column(Integer, primary_key=True)
-	object_id = Column(String(64), nullable=False)
-	first_name = Column(String(128), nullable=False)
-	last_name = Column(String(128), nullable=False)
-	birthplace = Column(String(128), nullable=True)
-	affiliation_name = Column(String(128), nullable=True)
-	# def __repr__(self):
-	# 	return "<People(first_name='%s', last_name='%s', birthplace='%s')>" % (self.first_name, self.last_name, self.birthplace)
-
-# cb_people_table = Table('cb_people', metadata,
-#             Column('id', Integer, primary_key=True),
-#             Column('object_id', String(64), nullable=False),
-#             Column('first_name', String(128), nullable=False),
-#             Column('last_name', String(128), nullable=False),
-#             Column('birthplace', String(128), nullable=True),
-#             Column('affiliation_name', String(128), nullable=True),           
-#         )
-
-
 if __name__ == "__main__":
-	""" Using inspector to get the 11 table names from schema '2013snapshot'
-	It is working, but is there a way to get the real data in each table 
-	just using inspector?
-	"""
-	inspector = inspect(engine)
-	table_name_list = []
-	
-	[table_name_list.append(SCHEMA_NAME + '.' + table_name) for table_name in inspector.get_table_names(SCHEMA_NAME)]
-	#print table_name_list
-
-
 	metadata = MetaData()
 	metadata.reflect(bind = engine, schema = SCHEMA_NAME)
-	cb_people_table = metadata.tables['2013snapshot.cb_people']
 
-	mapper(People, cb_people_table, non_primary=True)
+	# we can then produce a set of mappings from this MetaData.
+	Base = automap_base(metadata = metadata)
+	# calling prepare() just sets up mapped classes and relationships.
+	Base.prepare()
 
 	session = sessionmaker()
 	session.configure(bind = engine)
 	s = session()
+
+	People = Base.classes.cb_people
 	# find = select('*').select_from(People)
 	# rs = s.execute(find)
 	# allData = rs.fetchall() 
-	# print allData
+	#print allData
+	
+	# Find the people with first name 'Roni' in cb_peoples table
+	it = s.query(People).filter(People.first_name == 'Roni')
+	for i, itt in enumerate(it):
+		print itt.last_name
 
-	# Select the information on the people whose object_id equals 223303 in table cb_people
-	selecting = select([cb_people_table]).where(cb_people_table.c.object_id == 'p:223303')
-	# print select
-	rs = s.execute(selecting)
-	allData = rs.fetchall() 
-	print allData
 
-	# Select the information on the people who graduate from CMU in table cb_degrees_table
-	cb_degrees_table = metadata.tables['2013snapshot.cb_degrees']
-	selecting = select([cb_degrees_table]).where(cb_degrees_table.c.institution == 'Carnegie Mellon University')
-	rs = s.execute(selecting)
-	allData = rs.fetchall() 
-	print allData
-
-	# Get one row from table cb_funds
-	cb_funds_table = metadata.tables['2013snapshot.cb_funds']
-	selecting = select([cb_funds_table])
-	rs = s.execute(selecting)
-	allData = rs.fetchone() 
-	print allData
+	Relationships = Base.classes.cb_relationships
+	# Find the people with tile 'VP, GM & Editor In Chief - Clinical Decison Support (UpToDate)' in cb_relationships table
+	it = s.query(Relationships).filter(Relationships.title == 'VP, GM & Editor In Chief - Clinical Decison Support (UpToDate)')
+	for i, itt in enumerate(it):
+		print itt.person_object_id 
