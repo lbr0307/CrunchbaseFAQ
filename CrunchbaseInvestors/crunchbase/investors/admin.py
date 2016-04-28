f0f38ee49481dfd7958610e1710e94b2cea63672
lbr0307 <lbr0307@126.com>
2016-04-27 20:35:10 -0400

    Update admin.py

--- CrunchbaseInvestors/crunchbase/investors/admin.py
+++ CrunchbaseInvestors/crunchbase/investors/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from crunchbase.investors.models import Investor
+from crunchbase.investors.models import People, Investor
 
 admin.site.register(Investor)
+admin.site.register(People)
