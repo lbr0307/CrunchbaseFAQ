4759cb60913141555b5f2f4c705ae8d4a73cb115
lbr0307 <lbr0307@126.com>
2016-04-27 20:36:41 -0400

    Update schema.py

--- CrunchbaseInvestors/crunchbase/investors/schema.py
+++ CrunchbaseInvestors/crunchbase/investors/schema.py
@@ -1,37 +1,36 @@
-from crunchbase.investors.models import Investor
+from crunchbase.investors.models import People, Investor
 from graphene import ObjectType, relay
 from graphene.contrib.django.filter import DjangoFilterConnectionField
 from graphene.contrib.django.types import DjangoNode
 
+class PeopleNode(DjangoNode):
 
-# Graphene will automatically map the User model's fields onto the UserType.
-# This is configured in the UserType's Meta class (as you can see below)
-# class CategoryNode(DjangoNode):
-
-#     class Meta:
-#         model = Category
-#         filter_fields = ['name', 'ingredients']
-#         filter_order_by = ['name']
+    class Meta:
+        model = People
+        filter_fields = {
+            'name': ['exact', 'icontains', 'istartswith'],
+            'investors': ['exact', 'icontains', 'istartswith'],
+        }
+        # filter_fields = ['name', 'investors']
+        filter_order_by = ['name']
 
 
 class InvestorNode(DjangoNode):
 
     class Meta:
         model = Investor
-        # Allow for some more advanced filtering here
         filter_fields = {
             'name': ['exact', 'icontains', 'istartswith'],
-            #'notes': ['exact', 'icontains'],
-            #'category': ['exact'],
-            #'category__name': ['exact'],
             'institution': ['exact', 'icontains'],
+            'people': ['exact', 'icontains'],
+            'people__name': ['exact', 'icontains'],
         }
-        filter_order_by = ['name']
+        filter_order_by = ['name', 'people__name']
 
 
 class Query(ObjectType):
-    #category = relay.NodeField(CategoryNode)
-    #all_categories = DjangoFilterConnectionField(CategoryNode)
+    people = relay.NodeField(PeopleNode)
+    all_peoples = DjangoFilterConnectionField(PeopleNode)
 
     investor = relay.NodeField(InvestorNode)
     all_investors = DjangoFilterConnectionField(InvestorNode)
